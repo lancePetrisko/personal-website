6d1bb8167889a365b9fc9b1f8faaf3f8e4ed739c
lancePetrisko <petriskolance@gmail.com>
2026-05-29 20:26:05 -0600

    Moved homeServer and midnightHostting to play section

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+(function markActiveNavLink() {
+  var path = window.location.pathname;
+  document.querySelectorAll(".nav-center a, .nav-right a").forEach(function (a) {
+    var href = a.getAttribute("href");
+    if (!href || href.startsWith("#")) return;
+    var hrefPath = new URL(href, window.location.href).pathname;
+    if (path === hrefPath || path.endsWith("/" + hrefPath)) {
+      a.classList.add("nav-active");
+    }
+  });
+})();
+
 function alertButton() {
   alert("Halo!");
 }
