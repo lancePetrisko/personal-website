300946e9235e2cbbed3fd78e1295646a163a1945
lancePetrisko <petriskolance@gmail.com>
2026-05-29 20:28:01 -0600

    Updated navbar selected feature
    If you click one of the navbar options it will have it grayed out

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 (function markActiveNavLink() {
+  var allNavLinks = document.querySelectorAll(".nav-name, .nav-center a, .nav-right a");
+
+  function clearActive() {
+    allNavLinks.forEach(function (a) { a.classList.remove("nav-active"); });
+  }
+
+  // Highlight current page link on load
   var path = window.location.pathname;
   document.querySelectorAll(".nav-center a, .nav-right a").forEach(function (a) {
     var href = a.getAttribute("href");
@@ -7,6 +14,14 @@
     if (path === hrefPath || path.endsWith("/" + hrefPath)) {
       a.classList.add("nav-active");
     }
+  });
+
+  // Highlight any nav link on click (covers anchor links like #about, #experience)
+  allNavLinks.forEach(function (a) {
+    a.addEventListener("click", function () {
+      clearActive();
+      a.classList.add("nav-active");
+    });
   });
 })();
 
